d3a416317b6bbd276ee917e4111167683a9b0b12
Anisimov Nikita <anisimov0324@gmail.com>
2021-02-27 17:31:27 +0300

    Ready

--- script.py
+++ script.py
@@ -1,12 +1,20 @@
 import requests
-import numpy
+import json
+import numpy as np
 
 url = 'https://covid.ourworldindata.org/data/owid-covid-data.json'
-
+newCasesList = []
 
 if __name__ == "__main__":
+   inputPercentile = input('Введите перцентиль: ')
+   inputCountryCode = input('Введите код страны, по которому нужно собрать статистику: ')
    req = requests.get(url)
    if(req.status_code == 200):
        dataJSON = req.json()
-       for countryCodes in dataJSON:
-           print(countryCodes)
+       for countryCode in dataJSON:
+           if(inputCountryCode == countryCode):
+               countryData = dataJSON[inputCountryCode]['data']
+               for eachData in countryData:
+                   newCasesList.append(eachData['new_cases'])
+   percentileNewCases = np.percentile(newCasesList, float(inputPercentile))
+   print(percentileNewCases)
